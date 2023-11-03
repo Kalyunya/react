395885e92bb9b5c72567a1fe7d32b1f3e5468637
Kalyunya <kolya-bogolyubov@i.ua>
2023-11-03 20:15:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./page/home.js";
+import CardProduct from "./page/cardProduct.js";
+import Cart from "./page/cart.js";
+import Category from "./page/category.js";
+import Contact from "./page/contact.js";
+import DeleveryPayment from "./page/deleveryPayment.js";
+import AboutUs from './page/aboutUs.js';
+import WebsiteUsageInstructions from './page/websiteUsageInstructions.js';
+import SpecialistConsultation from './page/specialistConsultation.js';
+import Reviews from './page/reviews.js';
+import Smile from './page/smile.js';
+import MenuFooter from './component/footer/menu_footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/CardProduct" element={<CardProduct/>} />
+          <Route path="/Cart/:id" element={<Cart/>} />
+          <Route path="/Category/" element={<Category/>} />
+          <Route path="/Contact" element={<Contact/>} />
+          <Route path="/DeleveryPayment" element={<DeleveryPayment/>} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/WebsiteUsageInstructions" element={<WebsiteUsageInstructions />} />
+          <Route path="/SpecialistConsultation" element={<SpecialistConsultation />} />
+          <Route path="/Reviews" element={<Reviews />} />
+          <Route path="/Smile" element={<Smile />} />
+        </Routes>
+        <MenuFooter />
+      </BrowserRouter>
+    );
+  }
+  
+  export default App;
